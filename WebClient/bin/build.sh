09cf004f8acc9acf66d707a937fe3a6ef4568c5d
Yuriy Gorvitovskiy <yuriy@gorvitovskiy.com>
2017-06-25 22:09:15 -0700

    increase sensitive area of sub-action links in wide view

--- WebClient/bin/build.sh
+++ WebClient/bin/build.sh
@@ -5,4 +5,5 @@ echo Compiling Typescript
 
 echo Browserifying
 mkdir -p ./br
-./bin/browserify.js
+# ./bin/browserify.js
+./node_modules/.bin/browserify ./js/index.js --debug | ./node_modules/.bin/exorcist ./br/bundle.js.map > ./br/bundle.js
