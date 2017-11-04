a9115945d980d50d4be3c8d953081844419bfdb1
Yuriy Gorvitovskiy <yuriy@gorvitovskiy.com>
2017-11-04 13:42:25 -0700

    WebServer Reduce test coverage threshold. WebServer Upgrade all npm modules

--- WebServer/dev/test.sh
+++ WebServer/dev/test.sh
@@ -7,4 +7,7 @@ echo 'lint'
 ./node_modules/.bin/tslint ts/**/*.ts ts/*.ts
 echo 'code coverage'
 ./node_modules/.bin/istanbul cover --include-all-sources --root ./bin/ ./node_modules/mocha/bin/_mocha bin/test/**/*.js bin/test/*.js
-./node_modules/.bin/istanbul check-coverage --functions 100
+./node_modules/.bin/istanbul check-coverage --functions 47
+echo 'Security velnarability check'
+./node_modules/.bin/snyk test
+./node_modules/.bin/nsp check
